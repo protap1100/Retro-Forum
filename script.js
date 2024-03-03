b5285682c7bfe776ca6c554e2e75114f0eb7110b
protap1100 <protapb23@gmail.com>
2024-03-03 14:29:06 +0600

    post functionality added

--- script.js
+++ script.js
@@ -1,1 +1,73 @@
-console.log('Hello World')
+const  getAllData = async () =>{
+    const getData = await fetch('https://openapi.programming-hero.com/api/retro-forum/posts');
+    const post = await getData.json();
+    const allPost = post;
+    displayData(allPost);
+}
+
+function displayData(allPost){
+    const posts = allPost.posts;
+    
+    // using foreach to run over every element 
+    posts.forEach(data => {
+        // console.log(data);
+
+        const singlePost = document.getElementById('all-post');
+        const div = document.createElement('div');
+        div.classList = "flex-grow-0 flex gap-10 flex-col";
+        div.innerHTML= `
+                    <div class="flex  border border-[#181feb9b] bg-[#797DFC1A] flex-col lg:flex-row rounded-3xl p-10 gap-10">
+                    <div class="flex-grow-0 indicator">
+                        ${data.isActive ? '<span class="indicator-item badge bg-[#10b981]"></span>' : '<span class="indicator-item badge bg-[#ff3434]"></span>'};
+                        <div class="grid w-24 h-24 bg-base-300 place-items-center"><img src="${data.image}" alt=""></div>
+                    </div>
+                    <div class="flex-grow flex gap-6  flex-col"> 
+                    <div class="flex gap-5 text-[#12132DCC] font-medium">
+                        <h1> # ${data.category} </h1>
+                        <h1>Author:${data.author.name}</h1>
+                    </div>
+                    <div class="text-3xl font-bold">${data.title}</div>
+                    <div class="text-[#12132D99] ">${data.description}</div>
+                    <div class="border border-dashed border-gray-500"></div>
+
+                    <div class="flex justify-between items-center">
+                        <div class="flex gap-5 text-[#12132D99] ">
+                        <h1><i class="fa-regular fa-message pr-3"></i> ${data.comment_count}</h1>
+                        <h1><i class="fa-regular fa-eye pr-3"></i> ${data.view_count}</h1>
+                        <h1><i class="fa-regular fa-clock pr-3"></i>  </h1>
+                        </div>
+                        <div>
+                            <h1 class="text-2xl cursor-pointer" onclick="markAsRead('${data.title}', '${data.view_count}');"><i class="fa-regular fa-envelope bg-green-500"></i></h1>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+        `;
+
+        singlePost.appendChild(div);
+    });
+
+}
+
+let clickCount = 0;
+function markAsRead(title,view_count){
+    // console.log(title,view_count);
+    const markRead = document.getElementById('marking-read');
+    const div =document.createElement('div');
+    div.classList = 'flex justify-between px-3 gap-5 py-8 bg-white rounded-2xl';
+    div.innerHTML = `
+                <div>
+                    <h1 class="font-bold">${title}</h1>
+                </div>
+                <div>
+                    <h1><i class="fa-regular fa-eye pr-3 text-[#12132D99]"></i> ${view_count}</h1>
+                </div>
+    `;
+    markRead.appendChild(div);
+
+    clickCount++
+    const counter = document.getElementById('mark-count');
+    counter.innerText = clickCount;
+}
+
+getAllData();
